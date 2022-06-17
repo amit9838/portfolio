df0f6aa7aed61920958be65caeea33bfce98cbfc
Amit Chaudhary <amitchaudhry0539@gmail.com>
2022-06-18 01:49:40 +0530

    fixd messeging errors

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Hamburger Menu Items *********************************
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -10,7 +12,6 @@ hamburger.addEventListener("click", () => {
 let menuLinks = document.getElementsByClassName("nav-link");
 let arr;
 arr = Array.from(menuLinks);
-// console.log(arr);
 
 arr.forEach(n => n.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -18,6 +19,8 @@ arr.forEach(n => n.addEventListener("click", () => {
 }))
 
 
+
+// Srroll Smoothly *****************************
 window.scroll ({
     behavior : "smooth",
 })
@@ -60,7 +63,6 @@ function toExperience() {
             behavior:"smooth"
         })
     }
-
 }
 
 function toSkills() {
@@ -79,7 +81,6 @@ function toSkills() {
             behavior:"smooth"
         })
     }
-
 }
 
 function toContacts() {
@@ -100,18 +101,27 @@ function toContacts() {
 }
 
 
+// Display Message Delivery status ***************************** 
+let sent_alert = document.getElementById("msg-sent-alert")
+function msgAlert(msg){
+    sent_alert.style.visibility = "visible";
+    sent_alert.innerHTML = msg;
+
+    setTimeout(()=>{
+        sent_alert.style.visibility = "hidden";
+    },2000);
+}
+
+
 let send_msg_btn = document.getElementById("send-msg-btn")
-
+let newName = document.getElementById("name")
+let newEmail = document.getElementById("email")
+let newMessage = document.getElementById("message")
 let active = true;
-send_msg_btn.addEventListener('click', function() {
 
-    let newName = document.getElementById("name")
-    let newEmail = document.getElementById("email")
-    let newMessage = document.getElementById("message")
-    // send_msg_btn.disabled = true;
-    
-    // console.log(name);
-    // let subject = name;
+// Handling On click event to send message *******************************************
+send_msg_btn.addEventListener('click', function(e) {
+    e.preventDefault();
     // console.log(newName.value+" "+newEmail.value+ " "+newMessage.value)
     if(newName.value.length==0){
         newName.focus();
@@ -119,23 +129,16 @@ send_msg_btn.addEventListener('click', function() {
 
     if(newEmail.value.length==0){
         newName.focus();
-        
     }
 
     if(newMessage.value.length==0){
         newName.focus();
-        
     }
-
-
 
     if(newName.value.length>0 && newEmail.value.length>0 && newMessage.value.length>0){
         const data = {source:'portfolio',  name: newName.value, email : newEmail.value, message:newMessage.value};
-
-        
-        if(active){
-            // submitForm();
-            
+ 
+        if(active){            
             sendMsg(data);     
             // console.log("sending message")
             active = false;
@@ -144,46 +147,17 @@ send_msg_btn.addEventListener('click', function() {
         }
         else {
             // console.log("Waiting to send message.")
-        }
-        
-        let sent_alert = document.getElementById("msg-sent-alert")
-
-
-        
-        setTimeout(()=>{
-                newName.value = "";
-                newEmail.value = "";
-                newMessage.value = "";
-            
-            active = true;
-            send_msg_btn.classList.toggle("btn-loading")
-            sent_alert.style.visibility = "visible";
-            // console.log("Link active")
-
-            setTimeout(()=>{
-                sent_alert.style.visibility = "hidden";
-            },3000);
-
-        },4000);
-
-
-    }
-    
+        }    
+    } 
     else{
         console.log("All Fields are required!");
-    }
-    
+    } 
 })
 
 let msgForm = document.querySelector("#msg-form")
 
 
-// function submitForm() {
-
-
-// }
-
-
+// Sending Message Asyncronously ****************************************
 function sendMsg(data) {
     fetch('https://marca-msgapi.herokuapp.com/messages', {
         method: 'POST', // or 'PUT'
@@ -194,17 +168,40 @@ function sendMsg(data) {
       })
       .then(response => response.json())
       .then(data => {
-        console.log('Success:', "Message sent successfully");
+        
+        // console.log('Success:', "Message sent successfully");
+
+        setTimeout(()=>{
+            newName.value = "";
+            newEmail.value = "";
+            newMessage.value = "";
+        
+        active = true;
+        send_msg_btn.classList.toggle("btn-loading")
+        msgAlert("Message Sent!")
+        // console.log("Link active")
+
+        },3000);
       })
       .catch((error) => {
-        console.error('Error:', error);
+        // console.error('Error:', error);
+        
+        setTimeout(()=>{
+        
+        active = true;
+        send_msg_btn.classList.toggle("btn-loading")
+        msgAlert("Message not sent! Try again.");
+        // console.log("Link active")
+
+        },3000);
+
       });
       
 }
 
-window.addEventListener('scroll', scrollPos);
+// window.addEventListener('scroll', scrollPos);
 function scrollPos() {
-    console.log(this.scrollY);
+    // console.log(this.scrollY);
 }
 
 // console.log(screen.width);
